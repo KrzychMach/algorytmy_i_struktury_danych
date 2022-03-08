030788564872314736e5d7a6986ff01dcf63763f
KrzychMach <99477121+KrzychMach@users.noreply.github.com>
2022-03-08 16:06:06 +0100

    komentarze na starcie pliku

--- offline1/zad1.py
+++ offline1/zad1.py
@@ -1,9 +1,18 @@
 # Krzysztof Mach
+#
+# Z pierwszych k+1 elementów listy tworzę min heap (nie wiem jak po polsku niestety)
+# Najmniejszy element z heap (czyli dla min heap będzie to korzeń) dołączam do posortowanej listy
+# Dołączam do heap kolejny element, znowu robię min heap itd.
+#
+# Heap będę przechowywał w tablicy (pythonowa lista, ale u mnie będzie stałej długości)
+# dla elementu pod indeksem i:
+#   rodzic: (i - 1) // 2
+#   dzieci: 2 * i, 2 * i + 1
 from zad1testy import Node, runtests
 
 
-def SortH(p,k):
-    # tu prosze wpisac wlasna implementacje
+def SortH(p, k):
+
     pass
 
 
