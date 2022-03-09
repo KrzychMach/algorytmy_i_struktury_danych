fded5ba3e512336d8a08725bff169050695a4164
KrzychMach <99477121+KrzychMach@users.noreply.github.com>
2022-03-09 12:06:35 +0100

    9/10, ostatni test 1.02s

--- offline1/zad1.py
+++ offline1/zad1.py
@@ -3,47 +3,7 @@
 # Z pierwszych k+1 elementów listy tworzę min-heap (nie wiem jak po polsku niestety)
 # Najmniejszy element z heap (czyli dla min-heap będzie to korzeń) dołączam do posortowanej listy
 # Dołączam do heap kolejny element, znowu robię min-heap itd.
-#
-# Klasa oczywiście niepotrzebna, bo będzie tylko jeden heap, więc lepiej by było funkcjami, ale to tak na przyszłość
-# Poza tym lubię OOP w pythonie :)
 from zad1testy import Node, runtests
-
-
-class MinHeap(list):
-    def __init__(self, array):
-        super().__init__(array)
-        self.remaining = len(array)  # Niepotrzebne, mógłbym usuwać ostatnie elementy, ale nie wiem czy mogę używać del i append
-        self._minify()
-
-    def _minify(self):
-        changes = 1
-        while changes:  # wykonuje się, dopóki w poprzedniej iteracji zostały zamienione jakieś elementy
-            changes = 0
-            for i in range((self.remaining - 2) // 2, -1, -1):
-                try:
-                    if self[i].val > self[2 * i + 1].val:
-                        changes = 1
-                        self[i], self[2 * i + 1] = self[2 * i + 1], self[i]  # Zamiana dziecka i rodzica
-                        continue
-                    if self[i].val > self[2 * i + 2].val:
-                        changes = 1
-                        self[i], self[2 * i + 2] = self[2 * i + 2], self[i]  # Zamiana dziecka i rodzica
-                        continue
-                except IndexError:
-                    pass
-                except AttributeError:
-                    pass
-
-    def replace_last_node(self, new):
-        self[self.remaining - 1] = new
-        if new is None:
-            self.remaining -= 1
-        self._minify()
-
-    def get_min(self):
-        min_node = self[0]
-        self[0] = self[self.remaining - 1]
-        return min_node
 
 
 def SortH(p, k):
@@ -51,34 +11,64 @@ def SortH(p, k):
         return p
     elif p.next is None:
         return p
+    return heap_sort(p, k)
+
+
+# Tworzy listę instancji klasy Node (konkretniej odniesień do instancji) o długości size zaczynając od node
+# następnie nadaje mu odpowiednią strukturę, tj. najmniejszy element u góry
+def create_heap(node, size):
+    heap = [None for i in range(size)]
+    for i in range(size):
+        heap[i] = node
+        node = node.next
+        if node is None:
+            break
+    while heap[len(heap) - 1] is None:
+        del heap[len(heap) - 1]
+    for i in range((len(heap) - 1) // 2, -1, -1):  # nadaje poprawną strukturę
+        heapify(heap, i)
+    return heap
+
+
+# Przywraca poprawną strukturę, tj. najmniejszy element  w root
+def heapify(heap, root):
+    min_node = root
+    if 2 * root + 1 < len(heap) and heap[min_node].val > heap[2 * root + 1].val:
+        min_node = 2 * root + 1
+    if 2 * root + 2 < len(heap) and heap[min_node].val > heap[2 * root + 2].val:
+        min_node = 2 * root + 2
+    if min_node != root:
+        heap[root], heap[min_node] = heap[min_node], heap[root]  # najmniejszy element w roocie, stary root pod indeksem min_node
+        heapify(heap, min_node)  # rekurencyjne wywołanie dla starego roota
+
+
+def replace_root_node(heap, new):
+    if new is not None:
+        heap[0] = new
+    else:
+        heap[0] = heap[len(heap) - 1]
+        del heap[len(heap) - 1]
+    heapify(heap, 0)  # "Naprawiam" cały heap
+
+
+def heap_sort(p, k):
     min_heap = create_heap(p, k + 1)
-    for i in range(min_heap.remaining):
+    for i in range(len(min_heap)):
         p = p.next
     new_list_head = None
     curr_node = None
-    while min_heap.remaining > 0:
+    while min_heap:
         if new_list_head is not None:
-            curr_node.next = min_heap.get_min()
+            curr_node.next = min_heap[0]
             curr_node = curr_node.next
         else:
-            curr_node = min_heap.get_min()
+            curr_node = min_heap[0]
             new_list_head = curr_node
-        min_heap.replace_last_node(p)
+        replace_root_node(min_heap, p)
         if p is not None:
             p = p.next
     curr_node.next = None
     return new_list_head
 
 
-# Tworzy listę instancji klasy Node o długości size zaczynając od node, przekazuje ją do konstruktora MinHeap
-def create_heap(node, size):
-    heap_template = []
-    for i in range(size):
-        heap_template.append(node)
-        node = node.next
-        if node is None:
-            break
-    return MinHeap(heap_template)
-
-
-runtests( SortH )
+runtests(SortH)
